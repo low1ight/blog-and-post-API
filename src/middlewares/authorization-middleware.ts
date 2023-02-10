e59608fd302d793b566c7fdcae40518f5caed189
Maksym <qlowlight@gmail.com>
2023-02-10 10:58:31 +0200

    auth middleware bug fix

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -3,11 +3,11 @@ import {NextFunction, Request, Response} from "express";
 
 export const authorizationMiddleware = (req:Request,res:Response, next:NextFunction) => {
 
-    const encodedData = new Buffer("admin:qwerty").toString('base64')
+    const encodedData = Buffer.from("admin:qwerty").toString('base64')
     const correctLoginData = `Basic ${encodedData}`
 
-    if(req.headers.authorization === correctLoginData) next()
+    if(req.headers.authorization !== correctLoginData) res.send(401)
 
-    res.send(401)
+    next()
 
 }
