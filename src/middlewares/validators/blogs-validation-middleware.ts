64757e2eae45fd1615b1317874fda5bac460b2ae
Maksym <qlowlight@gmail.com>
2023-02-10 11:20:50 +0200

    add max url length to blog-validator

--- src/middlewares/validators/blogs-validation-middleware.ts
+++ src/middlewares/validators/blogs-validation-middleware.ts
@@ -19,12 +19,13 @@ export const BlogsValidationMiddleware = [
         .exists().withMessage(fieldErrorMessages.dontExist)
         .isString().withMessage(fieldErrorMessages.wrongType('string'))
         .isLength({ min: 1 }).withMessage(fieldErrorMessages.tooShort(1))
-        .isLength({ max: 15 }).withMessage(fieldErrorMessages.tooLong(500)),
+        .isLength({ max: 500 }).withMessage(fieldErrorMessages.tooLong(500)),
 
 
     body('websiteUrl').exists()
         .exists().withMessage(fieldErrorMessages.dontExist)
         .isString().withMessage(fieldErrorMessages.wrongType('string'))
+        .isLength({ max: 100 }).withMessage(fieldErrorMessages.tooLong(100))
         .matches('^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$'),
 ]
 
