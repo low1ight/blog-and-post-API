d6bcae676d4de149a0d07a99ad546a96188ccba6
Maksym <qlowlight@gmail.com>
2023-02-10 11:28:34 +0200

    remove min-length validate

--- src/middlewares/validators/posts-validation-middleware.ts
+++ src/middlewares/validators/posts-validation-middleware.ts
@@ -12,14 +12,12 @@ export const PostsValidationMiddleware = [
         .trim()
         .exists().withMessage(fieldErrorMessages.dontExist)
         .isString().withMessage(fieldErrorMessages.wrongType('string'))
-        .isLength({ min: 1 }).withMessage(fieldErrorMessages.tooShort(1))
         .isLength({ max: 30 }).withMessage(fieldErrorMessages.tooLong(30)),
 
     body('shortDescription').exists()
         .trim()
         .exists().withMessage(fieldErrorMessages.dontExist)
         .isString().withMessage(fieldErrorMessages.wrongType('string'))
-        .isLength({ min: 1 }).withMessage(fieldErrorMessages.tooShort(1))
         .isLength({ max: 100 }).withMessage(fieldErrorMessages.tooLong(100)),
 
 
@@ -27,7 +25,6 @@ export const PostsValidationMiddleware = [
         .trim()
         .exists().withMessage(fieldErrorMessages.dontExist)
         .isString().withMessage(fieldErrorMessages.wrongType('string'))
-        .isLength({ min: 1 }).withMessage(fieldErrorMessages.tooShort(1))
         .isLength({ max: 1000 }).withMessage(fieldErrorMessages.tooLong(1000)),
 
 
